5a5354525d5e56c201905664bd8a18c6afa6041b
MarthG <cgcarlos444@gmail.com>
2021-07-24 14:07:03 +0200

    Added ctime, include and json.hpp. Also added date processing

--- src/testing.cpp
+++ src/testing.cpp
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <ctime>
 #include <iostream>
 #include "menu.h"
 #include <curl/curl.h>
 #include <vector>
+#include "json.hpp"
+
+using json = nlohmann::json;
 
 size_t writeFunction(void *ptr, size_t size, size_t nmemb, std::string* data) {
     data->append((char*) ptr, size * nmemb);
@@ -57,7 +61,7 @@ int main(int argc, const char* argv[]) {
             curl_global_init(CURL_GLOBAL_DEFAULT);
             auto curl = curl_easy_init();
             if (curl) {
-                curl_easy_setopt(curl, CURLOPT_URL, "https://marf.xyz/archive?sql");
+                curl_easy_setopt(curl, CURLOPT_URL, "http://marf.xyz/archive?sql");
                 curl_easy_setopt(curl, CURLOPT_PORT, 8081L);
                 curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
                 curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 50L);
@@ -71,10 +75,20 @@ int main(int argc, const char* argv[]) {
                 curl_easy_setopt(curl, CURLOPT_HEADERDATA, &header_string);
 
                 curl_easy_perform(curl);
-                std::cout << response_string;
+                // std::cout << "got header: " << header_string << std::endl;
+                // std::cout << "response got: " << std::endl << response_string << std::endl;
                 curl_easy_cleanup(curl);
                 curl_global_cleanup();
                 curl = NULL;
+                if (response_string.length() > 0) {
+                    json test = json::parse(response_string);
+                    std::cout << "JSON dump is: " << std::endl << test.dump(4) << std::endl;
+                    // std::cout << test["now"] << std::endl;
+                    long int * epoch = new long int();
+                    *epoch = test["now"];
+                    std::time_t result = std::time(epoch);
+                    std::cout << "Time converted from JSON is: " << std::asctime(std::localtime(&result)) << std::endl;
+                }
                 
                 // std::cout << "header: " << header_string << std::endl;
                 // std::cout << response_string << std::endl;
